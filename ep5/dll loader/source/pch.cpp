52a66a29cdd7379fe4336ac1eb61330763a35148
426f776965 <67214185+426f776965@users.noreply.github.com>
2020-08-25 23:40:20 -0400

    Create pch.cpp

---
+++ ep5/dll loader/source/pch.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+
+void load_lib() {
+	//names are case-insensitive
+	LoadLibraryA("shaiyaeden");
+	LoadLibraryA("123123");
+	LoadLibraryA("skill");
+	LoadLibraryA("exp");
+	LoadLibraryA("sharekills");
+}
