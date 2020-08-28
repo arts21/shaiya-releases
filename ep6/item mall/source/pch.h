415d91b4862be8e891d6153a9290ad52431d1338
426f776965 <67214185+426f776965@users.noreply.github.com>
2020-08-28 03:13:39 -0400

    Update pch.h

--- ep6/item mall/source/pch.h
+++ ep6/item mall/source/pch.h
@@ -1,13 +1,12 @@
-#pragma once
+#ifndef PCH_H
+#define PCH_H
 
 #define WIN32_LEAN_AND_MEAN
-
-#include <sdkddkver.h>
 #include <windows.h>
 #include <atlstr.h>
 #include <comdef.h>
 #include <vector>
 
-BOOL Hook(void * pAddr, void * pNAddr, int len = 5);
-DWORD __stdcall ThreadProc1(_In_ LPVOID lpParameter);
-DWORD __stdcall ThreadProc2(_In_ LPVOID lpParameter);
+DWORD __stdcall DBConn(void*);
+
+#endif
