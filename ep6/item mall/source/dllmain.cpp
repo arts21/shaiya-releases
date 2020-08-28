f8c9772ab2475a02edfd973485c9e2c607dea9f3
426f776965 <67214185+426f776965@users.noreply.github.com>
2020-08-28 02:35:58 -0400

    Update dllmain.cpp

--- ep6/item mall/source/dllmain.cpp
+++ ep6/item mall/source/dllmain.cpp
@@ -1,12 +1,11 @@
 #include "pch.h"
-#include "database.h"
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
 {
 	switch (dwReason)
 	{
 	case DLL_PROCESS_ATTACH:
-		CreateThread(NULL, 0, ThreadProc1, 0, NULL, 0);
+		CreateThread(NULL, 0, DBConn, 0, NULL, 0);
 	case DLL_THREAD_ATTACH:
 	case DLL_THREAD_DETACH:
 	case DLL_PROCESS_DETACH:
