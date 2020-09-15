15f784af707c3aac04dd814faf02600fcd802a4c
426f776965 <67214185+426f776965@users.noreply.github.com>
2020-09-15 16:46:22 -0400

    Update pch.cpp

--- ep6/commands/pch.cpp
+++ ep6/commands/pch.cpp
@@ -26,7 +26,7 @@ message arguments
 14 = purple
 15 = light blue
 16 = light yellow
-posts as notice
+post as notice
 17 = white
 18 = yellow
 post in chat box
@@ -35,106 +35,108 @@ post in chat box
 */
 
 //asm hook for new user commands
-__declspec(naked) void cmdHook()
+__declspec(naked) void cmd_hook()
 {
-	_asm 
+	_asm
 	{
 		push 0x6 //push string length
 		push szClose //push cmd string
 		push edi
-		call cmdChk //check text input
+		call cmd_input
 		add esp,0xC
-		test eax,eax //test the result
+		test eax,eax
 		jne _market
 		//gives them the option to close window
 		mov dword ptr ds:[0x9144E4],0x0 //close
-		jmp cmdJMP
+		jmp cmd_jmp
 		_market:
-		push 0x7
+		push 0x9
 		push szMarket
 		push edi
-		call cmdChk
+		call cmd_input
 		add esp,0xC
 		test eax,eax
 		jne _keeper
 		//fail if a window is already open
 		cmp dword ptr ds:[0x9144E4],0x0
-		jne _cmdFail
+		jne _fail
 		//use a market npc without items
 		mov dword ptr ds:[0x91AD44],0x1 //type
 		mov dword ptr ds:[0x91AD40],0x12C //type id
 		mov dword ptr ds:[0x9144F0],-0x1 //icon
 		mov dword ptr ds:[0x22AB7B8],0x0 //name
 		mov dword ptr ds:[0x9144E4],0x65 //market
-		jmp cmdJMP
+		jmp cmd_jmp
 		_keeper:
 		push 0xA
 		push szKeeper
 		push edi
-		call cmdChk
+		call cmd_input
 		add esp,0xC
 		test eax,eax
 		jne _repair
 		cmp dword ptr ds:[0x9144E4],0x0
-		jne _cmdFail
+		jne _fail
 		mov dword ptr ds:[0x91AD44],0x6 //type	
 		mov dword ptr ds:[0x91AD40],0x1D //type id
 		mov dword ptr ds:[0x9144F0],-0x1 //icon
 		mov dword ptr ds:[0x22AB7B8],0x0 //name
 		mov dword ptr ds:[0x9144E4],0x67 //warehouse
-		jmp cmdJMP
+		jmp cmd_jmp
 		_repair:
-		push 0x7
+		push 0xB
 		push szRepair
 		push edi
-		call cmdChk
+		call cmd_input
 		add esp,0xC
 		test eax,eax
 		jne _reroll
 		cmp dword ptr ds:[0x9144E4],0x0
-		jne _cmdFail
+		jne _fail
 		mov dword ptr ds:[0x91AD44],0x3 //type
 		mov dword ptr ds:[0x91AD40],0x28 //type id
 		mov dword ptr ds:[0x9144F0],-0x1 //icon
 		mov dword ptr ds:[0x22AB7B8],0x0 //name
 		mov dword ptr ds:[0x9144E4],0x66 //blacksmith
-		jmp cmdJMP
+		jmp cmd_jmp
 		_reroll:
-		push 0x9
+		push 0xB
 		push szReroll
 		push edi
-		call cmdChk
+		call cmd_input
 		add esp,0xC
 		test eax,eax
-		jne _cmdExit
+		jne _original
 		cmp dword ptr ds:[0x9144E4],0x0
-		jne _cmdFail
+		jne _fail
 		mov dword ptr ds:[0x91AD44],0x1 //type
 		mov dword ptr ds:[0x91AD40],0xF8 //type id
 		mov dword ptr ds:[0x9144F0],-0x1 //icon
 		mov dword ptr ds:[0x22AB7B8],0x0 //name
 		mov dword ptr ds:[0x9144E4],0x79 //recreation
-		jmp cmdJMP
+		jmp cmd_jmp
 		//original code
-		_cmdExit:
-		push 0x13D4 // /return line in sysmsg-uni.txt
-		jmp cmdRet
+		_original:
+		push 0x13D4 // /return text
+		jmp cmd_retn
 		//send a notice
-		_cmdFail:
+		_fail:
 		push 0xC
 		push 0x326 //sysmsg-uni.txt line
-		push 0x1F //message argument
-		call sysMsg //send the notice
+		push 0x1F //message type argument
+		call sysmsg_uni //send the notice
 		add esp,0xC
-		jmp cmdJMP
+		jmp cmd_jmp
 	}
 }
 
 //main function
-void cmdFunc() {
-	Hook((void*)0x4867A1, cmdHook, 5);
+void CMDFunc() {
+	Hook((void*)0x4867A1, cmd_hook, 5);
+	//overwrite CustomGame.dll help menu npcs
+	memcpy((void*)0x522160, (void*)help_menu, sizeof(help_menu));
 	//automatically max item stack for fast sales
-	memcpy((void*)0x42E131, maxCnt, sizeof(maxCnt));
-	//ignores a function result (bug workaround)
-	memcpy((void*)0x444129, npcJMP, sizeof(npcJMP));
+	memcpy((void*)0x42E131, max_sell, sizeof(max_sell));
+	//ignores an npc function result (bug workaround)
+	memcpy((void*)0x444129, npc_jmp, sizeof(npc_jmp));
 }
