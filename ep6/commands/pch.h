629ce286fc87a926de6981c7c01e8393a7756d7e
426f776965 <67214185+426f776965@users.noreply.github.com>
2020-09-15 16:42:05 -0400

    Update pch.h

--- ep6/commands/pch.h
+++ ep6/commands/pch.h
@@ -4,20 +4,24 @@
 #define WIN32_LEAN_AND_MEAN 
 #include <windows.h>
 //main function
-void cmdFunc();
+void CMDFunc();
 //hook function
 BOOL Hook(void * pAddr, void * pNAddr, int len = 5);
-//variables and game.exe addresses
-DWORD cmdChk = 0x632D07;
-DWORD cmdRet = 0x4867A6;
-DWORD cmdJMP = 0x487532;
-DWORD sysMsg = 0x423150;
+//command variables
+DWORD cmd_input = 0x632D07;
+DWORD cmd_retn = 0x4867A6;
+DWORD cmd_jmp = 0x487532;
+DWORD sysmsg_uni = 0x423150;
 LPCSTR szClose = "/close";
-LPCSTR szMarket = "/market";
+LPCSTR szMarket = "/merchant";
 LPCSTR szKeeper = "/warehouse";
-LPCSTR szRepair = "/repair";
-LPCSTR szReroll = "/recreate";
-unsigned char maxCnt[] = { 0x90, 0x90 };
-unsigned char npcJMP[] = { 0xEB, 0x05 };
+LPCSTR szRepair = "/blacksmith";
+LPCSTR szReroll = "/recreation";
+unsigned char npc_jmp[] = { 0xEB, 0x05 };
+unsigned char max_sell[] = { 0x90, 0x90 };
+//CustomGame.dll help menu hook address
+unsigned char help_menu[] = { 0xE8, 0x0B, 0x9F, 0xFF, 0xFF };
+//CustomGame.dll command hook address
+unsigned char old_cmd[] = { 0x8B, 0x81, 0x2C, 0x01, 0x00, 0x00 };
 
 #endif
